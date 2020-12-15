655c86c146396dc5b6cde6815b6459eb8bac5c20
HelgaMoon <HelgaMoon>
2020-12-15 13:39:37 +0300

    Projects baseline

---
+++ RecycleMe/Application/Controllers/Controller.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Application.Controllers
+{
+	[ApiController]
+	[Route("[controller]")]
+	public class Controller : ControllerBase
+	{
+		private readonly ILogger<Controller> _logger;
+
+		public Controller(ILogger<Controller> logger)
+		{
+			_logger = logger;
+		}
+
+		[HttpGet]
+		public int Get()
+		{
+			return 1;
+		}
+	}
+}
